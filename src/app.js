7c21c44dcb10a649c5f8aadb88894eae4bfd6ecb
Emma <ep.dance@gmail.com>
2023-04-12 21:41:09 -0400

    first commits

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <h2>Welcome to my portfolio website!</h2>
+        <p>Here you can find information about me and my projects.</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
